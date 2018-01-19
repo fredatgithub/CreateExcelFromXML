3e6d93803bd86f6cc742d58239a69d56a7fb39a1
Freddy Juhel <domaine75@gmail.com>
2018-01-20 00:01:11 +0100

    Add xml file

--- XmlToExcel/FormMain.Designer.cs
+++ XmlToExcel/FormMain.Designer.cs
@@ -28,34 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(90, 88);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 244);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
+      this.buttonCreateExcel = new System.Windows.Forms.Button();
+      this.buttonReadXml = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // buttonCreateExcel
+      // 
+      this.buttonCreateExcel.Location = new System.Drawing.Point(60, 57);
+      this.buttonCreateExcel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.buttonCreateExcel.Name = "buttonCreateExcel";
+      this.buttonCreateExcel.Size = new System.Drawing.Size(83, 28);
+      this.buttonCreateExcel.TabIndex = 0;
+      this.buttonCreateExcel.Text = "create excel";
+      this.buttonCreateExcel.UseVisualStyleBackColor = true;
+      this.buttonCreateExcel.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // buttonReadXml
+      // 
+      this.buttonReadXml.Location = new System.Drawing.Point(180, 57);
+      this.buttonReadXml.Margin = new System.Windows.Forms.Padding(2);
+      this.buttonReadXml.Name = "buttonReadXml";
+      this.buttonReadXml.Size = new System.Drawing.Size(83, 28);
+      this.buttonReadXml.TabIndex = 1;
+      this.buttonReadXml.Text = "Read Xml";
+      this.buttonReadXml.UseVisualStyleBackColor = true;
+      this.buttonReadXml.Click += new System.EventHandler(this.buttonReadXml_Click);
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(517, 191);
+      this.Controls.Add(this.buttonReadXml);
+      this.Controls.Add(this.buttonCreateExcel);
+      this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.Name = "FormMain";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-    }
+        private System.Windows.Forms.Button buttonCreateExcel;
+    private System.Windows.Forms.Button buttonReadXml;
+  }
 }
 
--- XmlToExcel/FormMain.cs
+++ XmlToExcel/FormMain.cs
@@ -1,72 +1,118 @@
 ﻿using System;
 using System.Data;
+using System.Linq;
 using System.Windows.Forms;
 using System.Xml;
+using System.Xml.Linq;
+using XmlToExcel.Properties;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace XmlToExcel
 {
-    public partial class FormMain : Form
+  public partial class FormMain : Form
+  {
+    public FormMain()
     {
-        public FormMain()
-        {
-            InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Excel.Application xlApp;
-            Excel.Workbook xlWorkBook;
-            Excel.Worksheet xlWorkSheet;
-            object misValue = System.Reflection.Missing.Value;
-
-            DataSet ds = new DataSet();
-            XmlReader xmlFile;
-            int i = 0;
-            int j = 0;
-
-            //xlApp = new Excel.ApplicationClass();
-            xlApp = new Excel.ApplicationClass();
-            xlWorkBook = xlApp.Workbooks.Add(misValue);
-            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.Item[1];
-
-            xmlFile = XmlReader.Create("Product.xml", new XmlReaderSettings());
-            ds.ReadXml(xmlFile);
-
-            for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
-            {
-                for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
-                {
-                    xlWorkSheet.Cells[i + 1, j + 1] = ds.Tables[0].Rows[i].ItemArray[j].ToString();
-                }
-            }
-
-            xlWorkBook.SaveAs("xml2excel.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-            xlWorkBook.Close(true, misValue, misValue);
-            xlApp.Quit();
-
-            releaseObject(xlApp);
-            releaseObject(xlWorkBook);
-            releaseObject(xlWorkSheet);
-
-            MessageBox.Show("Done");
-        }
-
-        private void releaseObject(object obj)
-        {
-            try
-            {
-                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
-                obj = null;
-            }
-            catch (Exception ex)
-            {
-                obj = null;
-            }
-            finally
-            {
-                GC.Collect();
-            }
-        }
+      InitializeComponent();
     }
+
+    string[,] lines = new string[100, 5];
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      Excel.Application xlApp;
+      Excel.Workbook xlWorkBook;
+      Excel.Worksheet xlWorkSheet;
+      object misValue = System.Reflection.Missing.Value;
+
+      DataSet ds = new DataSet();
+      XmlReader xmlFile;
+      int i = 0;
+      int j = 0;
+
+      //xlApp = new Excel.ApplicationClass();
+      xlApp = new Excel.ApplicationClass();
+      xlWorkBook = xlApp.Workbooks.Add(misValue);
+      xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.Item[1];
+
+      xmlFile = XmlReader.Create("Product.xml", new XmlReaderSettings());
+      ds.ReadXml(xmlFile);
+
+      for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+      {
+        for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+        {
+          xlWorkSheet.Cells[i + 1, j + 1] = ds.Tables[0].Rows[i].ItemArray[j].ToString();
+        }
+      }
+
+      xlWorkBook.SaveAs("xml2excel.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+      xlWorkBook.Close(true, misValue, misValue);
+      xlApp.Quit();
+
+      releaseObject(xlApp);
+      releaseObject(xlWorkBook);
+      releaseObject(xlWorkSheet);
+
+      MessageBox.Show("Done");
+    }
+
+    private void releaseObject(object obj)
+    {
+      try
+      {
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+        obj = null;
+      }
+      catch (Exception)
+      {
+        obj = null;
+      }
+      finally
+      {
+        GC.Collect();
+      }
+    }
+
+    private void buttonReadXml_Click(object sender, EventArgs e)
+    {
+      XDocument xmlDoc;
+      try
+      {
+        string xmlFileName = "sample.xml";
+        xmlDoc = XDocument.Load(xmlFileName);
+      }
+      catch (Exception exception)
+      {
+        MessageBox.Show("error" + exception.Message);
+        return;
+      }
+
+      var result = from node in xmlDoc.Descendants("Issue")
+                   where node.HasElements
+                   let xElementAuthor = node.Element("Author")
+                   where xElementAuthor != null
+                   let xElementLanguage = node.Element("Language")
+                   where xElementLanguage != null
+                   let xElementQuote = node.Element("QuoteValue")
+                   where xElementQuote != null
+                   select new
+                   {
+                     authorValue = xElementAuthor.Value,
+                     languageValue = xElementLanguage.Value,
+                     sentenceValue = xElementQuote.Value
+                   };
+
+      foreach (var q in result)
+      {
+        if (!_allQuotes.ListOfQuotes.Contains(new Quote(q.authorValue, q.languageValue, q.sentenceValue)) &&
+            q.authorValue != string.Empty && q.languageValue != string.Empty && q.sentenceValue != string.Empty)
+        {
+          _allQuotes.Add(new Quote(q.authorValue, q.languageValue, q.sentenceValue));
+        }
+      }
+
+      _allQuotes.QuoteFileSaved = true;
+    }
+  }
 }
