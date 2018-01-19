7f2fa0877548d31fb35d1a39ec22ba24f1440b2a
JUHEL <freddy.juhel@sogeti.com>
2018-01-19 14:01:15 +0100

    Initial commit

---
+++ XmlToExcel/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace XmlToExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            DataSet ds = new DataSet();
+            XmlReader xmlFile;
+            int i = 0;
+            int j = 0;
+
+            //xlApp = new Excel.ApplicationClass();
+            xlApp = new Excel.ApplicationClass();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.Item[1];
+
+            xmlFile = XmlReader.Create("Product.xml", new XmlReaderSettings());
+            ds.ReadXml(xmlFile);
+
+            for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+                {
+                    xlWorkSheet.Cells[i + 1, j + 1] = ds.Tables[0].Rows[i].ItemArray[j].ToString();
+                }
+            }
+
+            xlWorkBook.SaveAs("xml2excel.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlApp);
+            releaseObject(xlWorkBook);
+            releaseObject(xlWorkSheet);
+
+            MessageBox.Show("Done");
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
